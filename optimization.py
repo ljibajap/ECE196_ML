633c936e99b144ff713974b35589c8cfb63e3edd
Luis Jibaja <ljibajap@ucsd.edu>
2021-04-28 08:05:40 -0700

    Final training and tunning, save best model on pickle file, save parse and process data used for training on pickle file

--- optimization.py
+++ optimization.py
@@ -26,6 +26,78 @@ def tunning_number_iterations(train_data,val_data,list_userId,list_movieId,num_t
     val_table = pd.DataFrame.from_dict(validation_dic)
     return (best_model,val_table)
 
+# Tunning the learning rate function
+def tunning_learning_rate(train_data,val_data,list_userId,list_movieId,numIter):
+    tune_learning_rate_list = np.array([0.0001, 0.00001, 0.00003, 0.00007 , 0.000001,0.000003, 0.000007],dtype=np.float64)
+    validation_loss_list = []
+    models = []
+    for i in range(len(tune_learning_rate_list)):
+        alpha = tune_learning_rate_list[i]
+        model = md.CF_Model(list_userId,list_movieId)
+        model.build_model(num_features=5,std=1)
+        model.train(train_data,val_data,num_iter=numIter,learning_rate=alpha,regulator=0.001)
+        min_validation = model.update_best_model()
+        validation_loss_list.append(min_validation)
+        models.append(model)
+        print("Learning Rate: " + str(alpha) + " - Validation Loss: " + str(round(min_validation,7)))
+    
+    min_validation_idx = np.argmin(validation_loss_list)
+    best_model = models[min_validation_idx]
+    
+    validation_dic = {'Learning Rate':tune_learning_rate_list, \
+        'Validation Loss': validation_loss_list}
+    val_table = pd.DataFrame.from_dict(validation_dic)
+
+    return (best_model,val_table)
+
+# Tunning the number of latent factors
+def tunning_latent_factors(train_data,val_data,list_userId,list_movieId,numIter,alpha,latent_factors):
+    tune_latentFactors_list = np.zeros(latent_factors,dtype=np.int)
+    validation_loss_list = []
+    models = []
+    for i in range(1,latent_factors + 1, 1):
+        k = 5 * i 
+        tune_latentFactors_list[i-1] = k
+        model = md.CF_Model(list_userId,list_movieId)
+        model.build_model(num_features=k,std=1)
+        model.train(train_data,val_data,num_iter=numIter,learning_rate=alpha,regulator=0.001)
+        min_validation = model.update_best_model()
+        validation_loss_list.append(min_validation)
+        models.append(model)
+        print("Num Latent Factors: " + str(k) + " - Validation Loss: " + str(round(min_validation,7)))
+
+    min_validation_idx = np.argmin(validation_loss_list)
+    best_model = models[min_validation_idx]
+
+    validation_dic = {'Latent Factors': tune_latentFactors_list, \
+        'Validation Loss': validation_loss_list}
+    val_table = pd.DataFrame.from_dict(validation_dic)
+    return (best_model,val_table)
+
+# Tunning the regulator 
+def tunning_regulator(train_data,val_data,list_userId,list_movieId,numIter,alpha,k):
+    tune_regulator_list = np.array([0.1, 0.01, 0.001, 0.0001 , 0.00001],dtype=np.float64)
+    validation_loss_list = []
+    models = []
+    for i in range(len(tune_regulator_list)):
+        omega = tune_regulator_list[i]
+        model = md.CF_Model(list_userId,list_movieId)
+        model.build_model(num_features=k,std=1)
+        model.train(train_data,val_data,num_iter=numIter,learning_rate=alpha,regulator=omega)
+        min_validation = model.update_best_model()
+        validation_loss_list.append(min_validation)
+        models.append(model)
+        print("Regulator: " + str(omega) + " - Validation Loss: " + str(round(min_validation,7)))
+    
+    min_validation_idx = np.argmin(validation_loss_list)
+    best_model = models[min_validation_idx]
+    
+    validation_dic = {'Regulator':tune_regulator_list, \
+        'Validation Loss': validation_loss_list}
+    val_table = pd.DataFrame.from_dict(validation_dic)
+
+    return (best_model,val_table)
+
 '''
 K-fold data break
 '''
@@ -47,7 +119,7 @@ def k_foldCV(data_set, k=10):
 
 # K-Cross Validation
 # Return the best model from the K fold
-def k_cross_validation(train_data, val_data, list_userId, list_movieId):
+def k_cross_validation(train_data, val_data, list_userId, list_movieId,k,alpha,numIter,omega):
     train_folds = k_foldCV(train_data, k=5)
     val_folds = k_foldCV(val_data,k=5)
     val_loss_list = []
@@ -56,8 +128,8 @@ def k_cross_validation(train_data, val_data, list_userId, list_movieId):
         train_holdout = train_folds[fold]
         val_holdout = val_folds[fold]
         model = md.CF_Model(list_userId,list_movieId)
-        model.build_model(num_features=5,std=1)
-        model.train(train_holdout,val_holdout,num_iter=70,learning_rate=0.00001,            regulator=0.001)
+        model.build_model(num_features=k,std=1)
+        model.train(train_holdout,val_holdout,num_iter=numIter,learning_rate=alpha,regulator=omega)
         min_validation = model.update_best_model()
         val_loss_list.append(min_validation)
         models.append(model)
